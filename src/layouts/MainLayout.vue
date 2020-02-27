d9e4a9d7a745ad12d298ad7445a313720da4d350
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-28 00:36:31 +0530

    cleanup

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,19 +13,19 @@
     q-drawer(v-model="leftDrawerOpen" :mini="miniState" :width="300" :breakpoint="0" bordered)
       q-scroll-area.fit
         q-list(padding)
-          q-item(v-for="(conversation, index) in conversations" :key="conversation.id" clickable="" v-ripple="" @click="currentConversationIndex = index")
-            q-item-section(avatar="")
+          q-item(v-for="(conversation, index) in conversations" :key="conversation.id" clickable v-ripple @click="currentConversationIndex = index")
+            q-item-section(avatar)
               q-avatar
                 img(:src="conversation.avatar")
             q-item-section
               q-item-label(lines="1")
                 | {{ conversation.person }}
-              q-item-label.conversation__summary(caption="")
+              q-item-label.conversation__summary(caption)
                 q-icon(name="check" v-if="conversation.sent")
                   q-icon(name="not_interested" v-if="conversation.deleted")
                     | {{ conversation.caption }}
-            q-item-section(side="")
-              q-item-label(caption="")
+            q-item-section(side)
+              q-item-label(caption)
                 | {{ conversation.time }}
               q-icon(name="keyboard_arrow_down")
 
@@ -33,25 +33,25 @@
 
     q-drawer(v-model="rightDrawerOpen" side="right" :width="250" :breakpoint="500")
       q-scroll-area.fit
-        q-list.menu-list(padding="")
-          q-item(clickable="" v-ripple="" @click="darkMode = !darkMode")
-            q-item-section(avatar="")
+        q-list.menu-list(padding)
+          q-item(clickable v-ripple @click="darkMode = !darkMode")
+            q-item-section(avatar)
               q-icon(name="inbox")
             q-item-section Dark Mode
             q-item-section.absolute-right
               q-toggle(color="green" v-model="darkMode")
-          q-item(active="" clickable="" v-ripple="")
-            q-item-section(avatar="")
+          q-item(active clickable v-ripple)
+            q-item-section(avatar)
               q-icon(name="star")
             q-item-section
               q-btn(color="primary" label="Trigger" @click="trigger")
-          q-item(clickable="" v-ripple="")
-            q-item-section(avatar="")
+          q-item(clickable v-ripple)
+            q-item-section(avatar)
               q-icon(name="send")
             q-item-section
               | Send
-          q-item(clickable="" v-ripple="")
-            q-item-section(avatar="")
+          q-item(clickable v-ripple)
+            q-item-section(avatar)
               q-icon(name="drafts")
             q-item-section
               | Drafts
@@ -60,7 +60,7 @@
     q-page-container
       router-view
     .q-pa-md
-      q-ajax-bar(ref="bar" position="bottom" color="accent" size="10px" skip-hijack="")
+      q-ajax-bar(ref="bar" position="bottom" color="accent" size="10px" skip-hijack)
 
 </template>
 
