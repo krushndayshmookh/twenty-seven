3ee2e9d6be10124e6a2be69e381524ed61f03305
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-27 16:23:33 +0530

    created prototype general chat

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,12 +1,73 @@
 <template lang="pug">
   q-page
-    q-input.q-ma-xs(filled v-model="text" label="Filled")
-    q-input.q-ma-xs(filled v-model="text" label="Filled")
+    .q-pt-sm
+      q-input.q-ma-xs(outlined readonly v-model="partnerMessage" placeholder="Partner")
+
+      q-input.q-ma-xs(outlined v-model="selfMessage" placeholder="You")
+        template(v-slot:append)
+          q-icon(v-if="selfMessage !== ''" name="close" @click="selfMessage = ''" class="cursor-pointer")
 
 </template>
 
 <script>
+import _ from 'lodash'
+import io from 'socket.io-client'
+
 export default {
-  name: "PageIndex"
-};
+  name: 'PageIndex',
+  data() {
+    return {
+      selfMessage: '',
+      partnerMessage: '',
+      connectionStatus: false
+    }
+  },
+  watch: {
+    selfMessage(val) {
+      this.debouncedSendMessage()
+    }
+  },
+  created() {
+    this.debouncedSendMessage = _.debounce(this.sendMessage, 500)
+  },
+  mounted() {
+    this.socket = io.connect('http://localhost:3000')
+    this.attachSocketListeners()
+  },
+  methods: {
+    sendMessage() {
+      this.socket.emit('msg', {
+        body: this.selfMessage,
+        to: 'krushn',
+        from: 'daffy'
+      })
+    },
+    attachSocketListeners() {
+      const vm = this
+
+      this.socket.on('connect', function() {
+        console.log('Socket connected.')
+        vm.connectionStatus = true
+        vm.doSocketLogin()
+      })
+
+      this.socket.on('disconnect', function() {
+        console.log('Socket disconnected.')
+        vm.connectionStatus = false
+      })
+
+      this.socket.on('msg', this.handleIncomingMessage)
+      // this.socket.on('stock data downloaded', function(data) {
+      //   vm.download.count = data.downloadCount
+      //   if (vm.download.count == vm.download.total) vm.download.status = false
+      //   console.log(vm.download, data)
+      // })
+    },
+    doSocketLogin() {},
+    handleIncomingMessage(data) {
+      console.log(data)
+      this.partnerMessage = data.body
+    }
+  }
+}
 </script>
