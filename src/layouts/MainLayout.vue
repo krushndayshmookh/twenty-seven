53fc83aff85974732b257bc23452850343ecf6eb
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-28 00:26:41 +0530

    added contact list

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,26 +13,22 @@
     q-drawer(v-model="leftDrawerOpen" show-if-above :mini="miniState" :width="300" :breakpoint="500" bordered)
       q-scroll-area.fit
         q-list(padding)
-          q-item(clickable v-ripple)
-            q-item-section(avatar)
-              q-icon(name="inbox")
+          q-item(v-for="(conversation, index) in conversations" :key="conversation.id" clickable="" v-ripple="" @click="currentConversationIndex = index")
+            q-item-section(avatar="")
+              q-avatar
+                img(:src="conversation.avatar")
             q-item-section
-              | Inbox
-          q-item(active clickable v-ripple)
-            q-item-section(avatar)
-              q-icon(name="star")
-            q-item-section
-              | Star
-          q-item(clickable v-ripple)
-            q-item-section(avatar)
-              q-icon(name="send")
-            q-item-section
-              | Send
-          q-item(clickable v-ripple)
-            q-item-section(avatar)
-              q-icon(name="drafts")
-            q-item-section
-              | Drafts
+              q-item-label(lines="1")
+                | {{ conversation.person }}
+              q-item-label.conversation__summary(caption="")
+                q-icon(name="check" v-if="conversation.sent")
+                  q-icon(name="not_interested" v-if="conversation.deleted")
+                    | {{ conversation.caption }}
+            q-item-section(side="")
+              q-item-label(caption="")
+                | {{ conversation.time }}
+              q-icon(name="keyboard_arrow_down")
+
 
 
     q-drawer(v-model="rightDrawerOpen" side="right" show-if-above="" :width="250" :breakpoint="500")
@@ -78,7 +74,41 @@ export default {
     return {
       leftDrawerOpen: false,
       rightDrawerOpen: false,
-      miniState: true
+      miniState: true,currentConversationIndex: 0,
+      conversations: [
+        {
+          id: 1,
+          person: 'Razvan Stoenescu',
+          avatar: 'https://cdn.quasar.dev/team/razvan_stoenescu.jpeg',
+          caption: 'I\'m working on Quasar!',
+          time: '15:00',
+          sent: true
+        },
+        {
+          id: 2,
+          person: 'Dan Popescu',
+          avatar: 'https://cdn.quasar.dev/team/dan_popescu.jpg',
+          caption: 'I\'m working on Quasar!',
+          time: '16:00',
+          sent: true
+        },
+        {
+          id: 3,
+          person: 'Jeff Galbraith',
+          avatar: 'https://cdn.quasar.dev/team/jeff_galbraith.jpg',
+          caption: 'I\'m working on Quasar!',
+          time: '18:00',
+          sent: true
+        },
+        {
+          id: 4,
+          person: 'Allan Gaunt',
+          avatar: 'https://cdn.quasar.dev/team/allan_gaunt.png',
+          caption: 'I\'m working on Quasar!',
+          time: '17:00',
+          sent: true
+        }
+      ]
     }
   },
 
