81337df98411d1ef83badddb22f76de7d19d5ccb
krushndayshmookh <krushndayshmookh@gmail.com>
2020-02-27 14:15:40 +0530

    init quasar and setup pug

---
+++ frontend/src/pages/Index.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  q-page
+    q-input.q-ma-xs(filled v-model="text" label="Filled")
+    q-input.q-ma-xs(filled v-model="text" label="Filled")
+
+</template>
+
+<script>
+export default {
+  name: "PageIndex"
+};
+</script>
